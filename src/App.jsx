26c01c8bf79da1ed228fbc3e54f89cfc88dbca04
manish7479dlp <manish7479dlp@gmail.com>
2024-01-13 19:56:25 +0530

    practice navbar

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ const App = () => {
   return (
     <>
       <NavBar />
-      <Hero />
+      {/* <Hero /> */}
     </>
   );
 };
--- src/pages/NavBar.jsx
+++ src/pages/NavBar.jsx
@@ -4,41 +4,38 @@ import { faBars, faXmark } from "@fortawesome/free-solid-svg-icons";
 import { faMoon } from "@fortawesome/free-regular-svg-icons";
 
 const NavBar = () => {
-    const [openNavLink , setOpenNavLink] = useState("hidden")
-    const toggleNavLink = () => {
-        setOpenNavLink((pre) => pre === 'hidden' ? 'none' : 'hidden')
-    }
+  const[navLink , setNavLink] = useState('hidden')
+
+  const toggleNavLink = () => {
+    setNavLink((pre) => pre === 'hidden' ? 'visible' : 'hidden')
+  }
   return (
-    <header className="bg-slate-800 w-full fixed top-0 left-0">
+    <header className="bg-slate-800 relative top-0 left-0 w-full z-10">
       <nav className="container h-14 flex justify-between items-center">
-        <div>
-          <h1 className="text-3xl font-bold">LOGO</h1>
-        </div>
+        
+          <h1 className="font-bold text-3xl uppercase">logo</h1>
 
-        <div className={`${openNavLink} w-full bg-slate-800 absolute top-0 left-0 py-14 md:block md:w-auto md:static md:py-0
-        md:ml-auto`}>
-          <ul className="flex flex-col text-center gap-5 text-xl md:flex-row">
+        <div className={`${navLink} py-14 bg-slate-800 w-full absolute top-0 left-0 text-2xl md:block md:py-0 md:w-auto md:static ml-auto`}>
+          <ul className="flex flex-col text-center font-semibold gap-5 md:flex-row">
             <li>Home</li>
-            <li>About</li>
             <li>Contact</li>
-            <li>Service</li>
             <li>Support</li>
+            <li>Privicy</li>
           </ul>
-          <div className="absolute top-[.7rem] right-4 text-2xl cursor-pointer">
-            <FontAwesomeIcon icon={faXmark} className="md:hidden" onClick={toggleNavLink}/>
-          </div>
-        </div>
-
-        <div className="flex items-center gap-5 text-2xl">
-          <FontAwesomeIcon icon={faMoon} className="cursor-pointer ml-4" />
 
           <div>
-            <FontAwesomeIcon icon={faBars} className="cursor-pointer md:hidden" onClick={toggleNavLink}/>
+            <FontAwesomeIcon icon={faXmark} className="cursonr-pointer text-3xl absolute top-[.7rem] right-4 md:hidden" onClick={toggleNavLink}/>
           </div>
+        </div>
+
+        <div className="flex gap-5 text-2xl">
+          <FontAwesomeIcon icon={faMoon} className="cursor-pointer ml-4"/>
+
+          <FontAwesomeIcon icon={faBars} className="cursor-pointer md:hidden" onClick={toggleNavLink}/>
         </div>
       </nav>
     </header>
-  );
-};
+  )
+}
 
-export default NavBar;
+export default NavBar
